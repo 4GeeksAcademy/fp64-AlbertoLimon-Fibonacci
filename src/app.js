88679daa64e96b41300e12ccd9ed367ce9f77eef
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-04-03 17:59:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const FIRST_POSITION = 0;
+const _getFibonacciNumber = (index) => {
+  if(index < 2){
+    return index;
+  }
+  return _getFibonacciNumber(index-1) + _getFibonacciNumber(index-2);
+}
+
+const _isLessThanZero = (number) => {
+  return number < 0;
+}
+const _printFibonacciNumber = (fibonacciNumber) => {
+  console.log(_getFibonacciNumber(fibonacciNumber));
+}
+
+const getFibonacciSequence = (positionQuantity) => {
+  if(_isLessThanZero(positionQuantity)){
+    return _getFibonacciNumber(FIRST_POSITION);
+  }else{
+    _printFibonacciNumber(positionQuantity);
+    return getFibonacciSequence(positionQuantity-1)
+  }
+}
+getFibonacciSequence(6)
 };
